38de1cc91d2595a2c45b00b4ae7a6fb56982e675
Kamil <kamil.wolak@interia.eu>
2025-01-15 12:11:48 +0100

    Added dbContext

---
+++ Ćwiczenie4_KamilWolak/DbConnection/CurrencyDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Ćwiczenie4_KamilWolak.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ćwiczenie4_KamilWolak.DbConnection;
+
+public class CurrencyDbContext : DbContext
+{
+    public CurrencyDbContext(DbContextOptions<CurrencyDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Rate> Rates { get; set; }
+    public DbSet<ExchangeTable> ExchangeTables { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ExchangeTable>()
+            .HasMany(x => x.Rates)
+            .WithOne();
+    }
+}
