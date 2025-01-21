d7f31683754ef4385ddb50227a38f7d3791bc9bd
Kamil <kamil.wolak@interia.eu>
2025-01-21 14:47:49 +0100

    During working with frontend unit test

--- Cwiczenie4_KamilWolak.UI/Cwiczenie4_KamilWolak.UI/src/app/currency-app/currency-app.component.spec.ts
+++ Cwiczenie4_KamilWolak.UI/Cwiczenie4_KamilWolak.UI/src/app/currency-app/currency-app.component.spec.ts
@@ -1,23 +1,61 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CurrencyAppComponent } from './currency-app.component';
+import { CurrencyService } from '../services/currency.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { CurrencyTableComponent } from '../currency-table/currency-table.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('CurrencyAppComponent', () => {
   let component: CurrencyAppComponent;
   let fixture: ComponentFixture<CurrencyAppComponent>;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CurrencyAppComponent]
-    })
-    .compileComponents();
+      imports: [
+        RouterTestingModule,
+        FormsModule,
+        CommonModule,
+        NgxChartsModule,
+        CurrencyAppComponent,  
+        CurrencyTableComponent
+      ],
+      providers: [
+        provideHttpClientTesting(),  // Nowoczesny sposób na zapewnienie mocka dla HttpClient
+        CurrencyService
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CurrencyAppComponent);
     component = fixture.componentInstance;
+    httpTestingController = TestBed.inject(HttpTestingController);
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call loadCurrenciesTable when button is clicked', () => {
+    spyOn(component, 'loadCurrenciesTable'); // Szpiegujemy metodę komponentu
+
+    // Znalezienie przycisku na podstawie klasy CSS
+    const button = fixture.nativeElement.querySelector('button.btn-primary');
+    expect(button).toBeTruthy();
+
+    // Kliknięcie przycisku
+    button.click();
+    fixture.detectChanges();
+
+    // Sprawdzenie, czy metoda została wywołana
+    expect(component.loadCurrenciesTable).toHaveBeenCalled();
+  });
+
+  afterEach(() => {
+    httpTestingController.verify(); // Sprawdzenie, czy nie ma oczekujących żądań HTTP
   });
 });
